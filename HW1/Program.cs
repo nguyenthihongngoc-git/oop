b7a4510374b2aeeb16ab07c600c1c666221245fd
nguyenthihongngoc-git <ngoc06hy@gmail.com>
2025-09-12 00:02:04 +0700

    Add HW1..HW5

---
+++ HW1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nguyen Thi Hong Ngoc\t12424024\t12424TN\tnguyenthihongngoc-git\tngoc06hy@gmail.com");
+        }
+    }
+}
